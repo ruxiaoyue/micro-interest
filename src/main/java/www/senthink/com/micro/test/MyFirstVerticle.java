7b3bcd704dea368914f41b59d0e5b91a5a4295d7
ruxiaoyue <hyacinth@senthink.com>
2017-12-12 09:57:07 +0800

    httpServer添加

---
+++ src/main/java/www/senthink/com/micro/test/MyFirstVerticle.java
@@ -0,0 +1,107 @@
+package www.senthink.com.micro.test;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.templ.JadeTemplateEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hyacinth on 2017/9/19.
+ */
+public class MyFirstVerticle extends AbstractVerticle{
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(MyFirstVerticle.class);
+
+    public void start(Future<Void> future) throws Exception{
+
+        final Router router = Router.router(vertx);
+
+        final MongoClient mongoClient = MongoClient.createShared(vertx, new JsonObject().put("db_name", "micro").put("host", "10.200.2.118"));
+
+        final JadeTemplateEngine jade = JadeTemplateEngine.create();
+
+        router.route().handler(BodyHandler.create());
+
+        router.get("/").handler(ctx ->{
+            ctx.put("title", "Vert.x.Web");
+
+            jade.render(ctx, "templates/index", res ->{
+                if (res.succeeded()) {
+                    ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "text/html").end(res.result());
+                }else {
+                    ctx.fail(res.cause());
+                }
+            });
+
+            router.get("/users").handler(context ->{
+                mongoClient.find("users", new JsonObject(), lookup -> {
+                    if (lookup.failed()) {
+                        ctx.fail(lookup.cause());
+                        return;
+                    }
+
+                    LOGGER.debug("find users size={}", lookup.result().size());
+                    final JsonArray json = new JsonArray();
+                    for (JsonObject o : lookup.result())
+                        json.add(o);
+
+                    ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+
+                    ctx.response().end(json.encode());
+                });
+            });
+        });
+
+
+        router.post("/users").handler(ctx -> {
+            JsonObject user = new JsonObject()
+                    .put("username", ctx.request().getFormAttribute("username"))
+                    .put("email", ctx.request().getFormAttribute("email"))
+                    .put("fullname", ctx.request().getFormAttribute("fullname"))
+                    .put("location", ctx.request().getFormAttribute("loaction"))
+                    .put("age", ctx.request().getFormAttribute("age"))
+                    .put("gender", ctx.request().getFormAttribute("gender"));
+
+            mongoClient.insert("users", user, lookup->{
+                if (lookup.failed()) {
+                    ctx.fail(lookup.cause());
+                    return;
+                }
+
+                ctx.response().setStatusCode(201);
+                ctx.response().end();
+
+            });
+        });
+
+        router.delete("/users/:id").handler(ctx -> {
+            mongoClient.removeDocument("users",
+                    new JsonObject().put("_id", ctx.request().getParam("id")), lookup ->{
+                if (lookup.failed()) {
+                    ctx.fail(lookup.cause());
+                    return;
+                }
+                ctx.response().setStatusCode(204);
+                ctx.response().end();
+            });
+        });
+
+
+       router.route().handler(StaticHandler.create());
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080, handler ->{
+            if (handler.succeeded()) {
+                LOGGER.debug("httpServer listen 8080");
+            }
+        });
+
+    }
+}
---
+++ src/main/java/www/senthink/com/micro/test/runner.java
@@ -0,0 +1,28 @@
+package www.senthink.com.micro.test;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by lenovo on 2017/9/19.
+ */
+public class runner {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(runner.class);
+
+    public static void main(String[] arg) {
+        Vertx vertx = Vertx.vertx();
+        DeploymentOptions options = new DeploymentOptions();
+        options.setConfig(new JsonObject().put("host", "0.0.0.0").put("port", 8080));
+        vertx.deployVerticle(MyFirstVerticle.class.getCanonicalName(), options, result ->{
+            if (result.succeeded()) {
+                LOGGER.debug("verticle deploy success");
+            }else {
+                LOGGER.debug("Verticle deploy faield ::{}", result.cause().getLocalizedMessage());
+            }
+        });
+    }
+}
